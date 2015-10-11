c04a110431d1e478a46a1b717e7539f839310bb4
Milind Mahajan <milind.mahajan@sjsu.edu>
2015-10-10 18:27:09 -0700

    Initial flow for youtube data API integration
    Signed-off-by: Milind Mahajan <milind.mahajan@sjsu.edu>

---
+++ app/src/main/java/com/example/milindmahajan/connectionutil/YouTubeConnector.java
@@ -0,0 +1,71 @@
+package com.example.milindmahajan.connectionutil;
+
+import android.content.Context;
+
+import com.example.milindmahajan.model.File;
+import com.example.milindmahajan.mytube.R;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by milind.mahajan on 10/10/15.
+ */
+public class YouTubeConnector {
+
+    private YouTube youtube;
+    private YouTube.Search.List query;
+
+    // Your developer key goes here
+    public static final String KEY
+            = "AIzaSQZZQWQQWMGziK9H_qRxz8g-V6eDL3QW_Us";
+
+    public YouTubeConnector(Context context) {
+        youtube = new YouTube.Builder(new NetHttpTransport(),
+                new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest hr) throws IOException {}
+        }).setApplicationName("My Tube").build();
+
+        try{
+            query = youtube.search().list("id,snippet");
+            query.setKey(KEY);
+            query.setType("video");
+            query.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+        }catch(IOException e){
+
+            e.printStackTrace();
+        }
+    }
+
+    public List<File> search(String keywords){
+        query.setQ(keywords);
+        try{
+            SearchListResponse response = query.execute();
+            List<SearchResult> results = response.getItems();
+
+            List<File> items = new ArrayList<File>();
+            for(SearchResult result:results){
+                File item = new File();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setDescription(result.getSnippet().getDescription());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getId().getVideoId());
+                items.add(item);
+            }
+            return items;
+        }catch(IOException e){
+
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/example/milindmahajan/mytube/SearchFragment.java
+++ app/src/main/java/com/example/milindmahajan/mytube/SearchFragment.java
@@ -1,46 +1,111 @@
 package com.example.milindmahajan.mytube;
 
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.milindmahajan.connectionutil.YouTubeConnector;
+import com.example.milindmahajan.model.File;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by milind.mahajan on 10/4/15.
  */
 public class SearchFragment extends Fragment {
 
+    View rootView;
+    private Handler handler;
+    private ListView videosFound;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        handler = new Handler();
 
-        EditText searchEditText = (EditText) rootView.findViewById(R.id.search_edit_text);
+        EditText searchEditText = (EditText) rootView.findViewById(R.id.search_input);
 
-        searchEditText.addTextChangedListener(new TextWatcher() {
-            
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 
-            }
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
 
-            @Override
-            public void afterTextChanged(Editable s) {
+                    searchOnYoutube(v.getText().toString());
 
+                    return false;
+                }
+
+                return true;
             }
         });
 
         return rootView;
+    }
+
+    private List<File> searchResults;
+
+    private void searchOnYoutube(final String keywords) {
+
+        new Thread(){
+
+            public void run(){
+
+                YouTubeConnector yc = new YouTubeConnector(rootView.getContext());
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable(){
+
+                    public void run(){
+
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void updateVideosFound() {
+
+        ArrayAdapter<File> adapter = new ArrayAdapter<File>(rootView.getContext(), R.layout.search_item, searchResults) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                if(convertView == null){
+
+                    convertView = getActivity().getLayoutInflater().inflate(R.layout.search_item, parent, false);
+                }
+
+                ImageView thumbnail = (ImageView)convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView)convertView.findViewById(R.id.video_title);
+                TextView description = (TextView)convertView.findViewById(R.id.video_description);
+
+                File searchResult = searchResults.get(position);
+
+                Picasso.with(rootView.getContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
     }
 }
